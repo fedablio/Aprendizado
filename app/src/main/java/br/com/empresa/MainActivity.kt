b202410220bdb8ba75be31ca3025faa0608a4602
fedablio <fw_150@hotmail.com>
2022-04-14 09:25:40 -0300

    Update MainActivity.kt

--- app/src/main/java/br/com/empresa/MainActivity.kt
+++ app/src/main/java/br/com/empresa/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //Uninter 14/04/2022 09:16
 //Comentário de alteração
+//Esse é um super código
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
