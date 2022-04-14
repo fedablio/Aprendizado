ef6b67bc714a0eca0b4d36bf681703e7c29c1ddc
fedablio <fw_150@hotmail.com>
2022-04-14 09:15:48 -0300

    Alteração Teste 2

---
+++ app/src/main/java/br/com/empresa/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.empresa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//Teste 1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
